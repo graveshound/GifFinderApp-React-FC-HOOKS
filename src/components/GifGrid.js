c42ad0fd69055bceffecb80e669cd762c9f5120e
TofuBoy <grandfunk30@gmail.com>
2021-07-16 15:52:45 -0500

    useEffect to FETCH API GIPHY

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const GifGrid = ({category}) => {
+
+    const [count, setCount]=useState(0);
+    useEffect(()=>{
+        getGifs();
+    },[])
     const API_KEY='2vvv5c6ZhXIOqwnlIF6D3WsA6LLkC79b';
     const getGifs = async() =>{
 
@@ -19,12 +24,14 @@ export const GifGrid = ({category}) => {
         })
         console.log(gifs)
     }
-    getGifs();
+
     
     
     return (
         <div>
             <h3>{category}</h3>
+            <h3>{count}</h3>
+            <button onClick={()=>{setCount(count+1)}}>Incrementar</button>
         </div>
     )
 }

d20f1b1cfbc6fab39cfec0947bd543229ada08c4
TofuBoy <grandfunk30@gmail.com>
2021-07-16 14:16:01 -0500

    setCategories system for Finder

--- src/GifFinderApp.js
+++ src/GifFinderApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export const GifFinderApp = () => {
+
+    //const categories=['Anime','Crypto','Rock']
+    const [categories,setCategories]=useState(['Anime','Crypto','Rock'])
+
+   const handleAdd =()=>{
+        //categories.push('Software');
+        //setCategories([...categories, 'Software'])
+        setCategories((cats)=>[...cats,'Software'])
+        console.log(categories)
+    }
+    console.log(categories)
     return (
         <>
             <h2>Gif Finder App</h2>
             <hr />
+            <button onClick={handleAdd}>Agregar Categoria</button>
+            <ol>
+                { 
+                    categories.map((category)=>{
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </>
     )
 }
